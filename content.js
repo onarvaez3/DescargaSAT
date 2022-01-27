d81df71108bfa84d8e71e0acc8ba03f3b4e2469c
onarvaez3 <57116624+onarvaez3@users.noreply.github.com>
2022-01-27 09:17:38 -0600

    AcuseFinal added

--- content.js
+++ content.js
@@ -4,37 +4,90 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     {
         if(request.data == "full")
         {
-            var downloadLinks = { pdfDownloadLinks: new Array(), xmlDownloadLinks: new Array(), fileNames: new Array() };
+            var result = { 
+                "records": new Array(),
+                "pdfCount": 0,
+                "xmlCount": 0,
+                "cancelCount": 0
+            };
+            let recordCounter = 0;
+            let nameCounter = 0;
 
-            var riElements = document.getElementsByName('BtnRI');
-            for (var i = 0; i < riElements.length; i++) 
+            var tableRows = document.getElementById("ctl00_MainContent_tblResult").firstElementChild.children;
+            for (const tr of tableRows)
             {
-                var relURL = riElements[i].getAttribute('onclick');
-                if(relURL.length > 0) 
-                {
-                    var downloadParams = relURL.match("\'([^']+)\'")[1];
-                    downloadLinks.pdfDownloadLinks.push("https://portalcfdi.facturaelectronica.sat.gob.mx/RepresentacionImpresa.aspx?Datos=" + downloadParams);
+                let newRecord = {
+                    "id": recordCounter
                 }
-                else
-                {
-                    console.error('Element is missing onclick event');
+                if(tr.firstElementChild && tr.firstElementChild.tagName == "TD") {
+                    if(tr.firstElementChild.firstElementChild && tr.firstElementChild.firstElementChild.tagName == "DIV")
+                    {
+                        if(tr.firstElementChild.firstElementChild.firstElementChild && tr.firstElementChild.firstElementChild.firstElementChild.tagName == "TABLE")
+                        {
+                            let innerTable = tr.firstElementChild.firstElementChild.firstElementChild;
+                            if(innerTable.firstElementChild && innerTable.firstElementChild.tagName == "TBODY")
+                            {
+                                for(const td of innerTable.firstElementChild.firstElementChild.children)
+                                {
+                                    if(td.hasChildNodes)
+                                    {
+                                        let child = td.firstElementChild;
+                                        if(child && child.hasAttribute("id"))
+                                        {
+                                            switch (child.getAttribute("id")) {
+                                                case "BtnDescarga":
+                                                    var relURL = child.getAttribute('onclick');
+                                                    if(relURL.length > 0) 
+                                                    {
+                                                        var downloadParams = relURL.match("\'([^']+)\'")[1];
+                                                        newRecord["xmlDownloadLink"] = "https://portalcfdi.facturaelectronica.sat.gob.mx/" + downloadParams;
+                                                        result.xmlCount++;
+                                                    }
+                                                    else
+                                                    {
+                                                        console.error('Element is missing onclick event');
+                                                    }
+                                                    break;
+                                                case "BtnRI":
+                                                    var relURL = child.getAttribute('onclick');
+                                                    if(relURL.length > 0) 
+                                                    {
+                                                        var downloadParams = relURL.match("\'([^']+)\'")[1];
+                                                        newRecord["pdfDownloadLink"] = "https://portalcfdi.facturaelectronica.sat.gob.mx/RepresentacionImpresa.aspx?Datos=" + downloadParams;
+                                                        result.pdfCount++;
+                                                    }
+                                                    else
+                                                    {
+                                                        console.error('Element is missing onclick event');
+                                                    }
+                                                    break;
+                                                case "BtnRecuperaAcuseFinal":
+                                                    var relURL = child.getAttribute('onclick');
+                                                    if(relURL.length > 0) 
+                                                    {
+                                                        var downloadParams = relURL.match("\'([^']+)\'")[1];
+                                                        newRecord["cancelDownloadLink"] = "https://portalcfdi.facturaelectronica.sat.gob.mx/" + downloadParams;
+                                                        result.cancelCount++;
+                                                    }
+                                                    else
+                                                    {
+                                                        console.error('Element is missing onclick event');
+                                                    }
+                                                    break;
+                                            
+                                                default:
+                                                    break;
+                                            }
+                                        }
+                                    }
+                                }
+                                result.records.push(newRecord);
+                                recordCounter++;
+                            }
+                        }
+                    }
                 }
-            }
-
-            var xmlElements = document.getElementsByName('BtnDescarga');
-            for (var i = 0; i < xmlElements.length; i++) 
-            {
-                var relURL = xmlElements[i].getAttribute('onclick');
-                if(relURL.length > 0) 
-                {
-                    var downloadParams = relURL.match("\'([^']+)\'")[1];
-                    downloadLinks.xmlDownloadLinks.push("https://portalcfdi.facturaelectronica.sat.gob.mx/" + downloadParams);
-                }
-                else
-                {
-                    console.error('Element is missing onclick event');
-                }
-            }
+            };
 
             var tds = document.getElementById("ContenedorDinamico").getElementsByTagName('td');
             for(var i = 0; i<tds.length;i++)
@@ -47,12 +100,18 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
                         i++;
                         var rfc_emisor = tds[i].textContent;
 
-                        downloadLinks.fileNames.push(rfc_emisor.substring(0,3) + "-" + uuid.substring(0,8));
+                        result.records[nameCounter]["filename"] = rfc_emisor.match('[a-zA-Z]+')[0] + "-" + uuid.substring(0,8);
+                        nameCounter++;
                     }
                 }
             }
 
-            sendResponse(downloadLinks);
+            if(recordCounter == nameCounter)
+            {
+                sendResponse(result);
+            } else {
+                sendResponse("Counters mismatch");
+            }
         }
         else if(request.data = "count")
         {
